714469f273f9d55ebc693741d0e71e3d88a6f1c1
Attila Szucs <attila.szucs@flixbus.com>
2020-01-26 13:00:51 +0100

    initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "spark-workshop"
+
+version := "0.1"
+
+scalaVersion := "2.12.10"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
---
+++ src/main/scala/com/uniobuda/sparkworkshop/Program.scala
@@ -0,0 +1,27 @@
+package com.uniobuda.sparkworkshop
+
+import org.apache.spark.SparkContext
+import scala.reflect.io.Directory
+import java.io.File
+
+object Program extends App {
+
+  val homeFolderName = System.getProperty("user.home")
+  val outputFolderName = s"$homeFolderName/wordcount"
+  val outputFolder = new Directory(new File(outputFolderName))
+  outputFolder.deleteRecursively()
+
+  val sc = new SparkContext("local[*]", "SparkWorkshop")
+  val resultRDD = sc.textFile("file:///etc/hosts", 4)
+    .flatMap(line => line.split(" "))
+    .map(word => (word, 1))
+    .reduceByKey((a,b) => a + b)
+
+  resultRDD
+    .saveAsTextFile(s"file://$outputFolderName")
+
+  resultRDD.collect.foreach {
+    case (word, count) => println(s"$word: $count")
+  }
+
+}
